9cd9670da0ed0aa8a07c47f010d64650b58215d9
Govindarajan Panneerselvam <imgovind@live.com>
2014-10-26 12:48:04 -0500

    Better Naming Convention -- Reserving Generic Package Names for comprehensive packages.

---
+++ Test.Framework.Cache.Core/NullCacher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Framework.Cache
+{
+    public class NullCacher : ICacher
+    {
+        public int Count
+        {
+            get { return 0; }
+        }
+
+        public T Get<T>(string key)
+        {
+            return default(T);
+        }
+
+        public void Remove(string key)
+        {
+        }
+
+        public bool Contains(string key)
+        {
+            return false;
+        }
+
+        public List<string> CachedKeys
+        {
+            get { return null;}
+        }
+
+        public void Set<T>(string key, T value)
+        {
+        }
+
+        public void Set<T>(string key, T value, DateTime absoluteExpiration)
+        {
+        }
+
+        public void Set<T>(string key, T value, TimeSpan slidingExpiration)
+        {
+        }
+
+        public bool TryGet<T>(string key, out T value)
+        {
+            value = default(T);
+            return false;
+        }
+    }
+}
