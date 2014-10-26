bc7567d1cf85757dcc8639ec8bc136852ec3ff26
Govindarajan Panneerselvam <imgovind@live.com>
2014-10-26 17:57:52 -0500

    Added NuSpec file for Cache InProc Modified AssemblyInfo for use in the nuspec file Added Source Transform file for App Initializer

---
+++ Test.Framework.Cache.InProc/Content/App_Start/InProcCache.cs.pp
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Framework;
+using Test.Framework.Cache;
+using Test.Framework.Cache.InProc;
+
+namespace $rootnamespace$
+{
+    public partial class AppInitializer
+    {
+        public partial class InProcCache
+        {
+            public static void Register()
+            {
+                Container.Register<ICacher, InProcCacher>(FrameworkSettings.IocKeys.InProcCacherKey);
+            }
+
+            public static void Initialize()
+            {
+                Cacher.InitializeInProc(Container.Resolve<ICacher>(FrameworkSettings.IocKeys.InProcCacherKey));
+            }
+        }
+    }
+}
