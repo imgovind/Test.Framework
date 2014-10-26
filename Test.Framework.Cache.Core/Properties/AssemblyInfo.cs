03577da954d13be489be1de85886896ae0d8dd28
Govindarajan Panneerselvam <imgovind@live.com>
2014-10-26 13:29:29 -0500

    Added NuSpec file for Cache Core Modified AssemblyInfo for use in the nuspec file

--- Test.Framework.Cache.Core/Properties/AssemblyInfo.cs
+++ Test.Framework.Cache.Core/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Test.Framework.Cache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Components to help build a concrete implementation of Caching for Framework Core.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Govindarajan Panneerselvam")]
 [assembly: AssemblyProduct("Test.Framework.Cache")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
