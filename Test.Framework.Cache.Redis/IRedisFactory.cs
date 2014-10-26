601d5f060538d460c425eee7e55884b8cb245f00
Govindarajan Panneerselvam <imgovind@live.com>
2014-10-26 17:59:03 -0500

    Added NuSpec file for Cache Redis Added Source Transform file for App Initializer -- Modified AssemblyInfo for use in the nuspec file Modified the namespace from Caching to Cache

--- Test.Framework.Cache.Redis/IRedisFactory.cs
+++ Test.Framework.Cache.Redis/IRedisFactory.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test.Framework.Caching.Redis
+namespace Test.Framework.Cache.Redis
 {
     public interface IRedisFactory
     {
---
+++ Test.Framework.Cache.Redis/content/App_Start/RedisCache.cs.pp
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Framework;
+using Test.Framework.Cache;
+using Test.Framework.Cache.Redis;
+
+namespace $rootnamespace$
+{
+    public partial class AppInitializer
+    {
+        public partial class RedisCache
+        {
+            public static void Register()
+            {
+                Container.Register<IRedisFactory, RedisFactory>();
+                FrameworkSettings.IocKeys.OutProcCacherKey = FrameworkSettings.IocKeys.RedisCacherKey;
+                Container.Register<ICacher, RedisCacher>(FrameworkSettings.IocKeys.RedisCacherKey);
+            }
+
+            public static void Initialize()
+            {
+                Cacher.InitializeOutProc(Container.Resolve<ICacher>(FrameworkSettings.IocKeys.RedisCacherKey));
+            }
+        }
+    }
+}
