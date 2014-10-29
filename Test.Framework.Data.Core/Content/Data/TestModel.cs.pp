66fa7aa1c72c18e3db58481dd0259c4f0c34df6d
Govindarajan Panneerselvam <imgovind@live.com>
2014-10-29 01:23:46 -0500

    Updated Model Register

---
+++ Test.Framework.Data.Core/Content/Data/TestModel.cs.pp
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $rootnamespace$.Data
+{
+    public class TestModel
+    {
+        public string TestStringProperty { get; set; }
+        public int TestIntProperty { get; set; }
+        public Guid TestGuidProperty { get; set; }
+    }
+}
---
+++ Test.Framework.Data.Core/Content/Data/TestModelMapper.cs.pp
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace $rootnamespace$.Data
+{
+    public class TestModelMapper : ISelectable<TestModel>
+    {
+        public TestModel ApplySelect(DataReader reader)
+        {
+            return new TestModel 
+            { 
+                TestStringProperty = reader.GetStringNullable("TestStringPropertyColumnName"),
+                TestGuidProperty = reader.GetGuid("TestGuidPropertyColumnName"),
+                TestIntProperty = reader.GetInt("TestIntPropertyColumnName")
+            };
+        }
+
+        public TestModel ApplySelect(DataReader reader, ISet<string> columns)
+        {
+            var result = new TestModel();
+
+            if (columns.Contains("TestStringPropertyColumnName")) result.TestStringProperty = reader.GetStringNullable("TestStringPropertyColumnName");
+            if (columns.Contains("TestIntPropertyColumnName")) result.TestIntProperty = reader.GetInt("TestIntPropertyColumnName");
+            if (columns.Contains("TestGuidPropertyColumnName")) result.TestGuidProperty = reader.GetGuid("TestGuidPropertyColumnName");
+
+            return result;
+        }
+    }
+}
