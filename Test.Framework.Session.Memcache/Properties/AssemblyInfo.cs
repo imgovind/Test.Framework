b53b08d0f071ee02820fbedc2c733b4d9dfa8a9d
Govindarajan Panneerselvam <imgovind@live.com>
2014-11-12 00:51:55 -0600

    Modified AssemblyInfo for use in the nuspec file

--- Test.Framework.Session.Memcache/Properties/AssemblyInfo.cs
+++ Test.Framework.Session.Memcache/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Test.Framework.Session.Memcache")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Components for using Memcache as a Session Store")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Govindarajan Panneerselvam")]
 [assembly: AssemblyProduct("Test.Framework.Session.Memcache")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
