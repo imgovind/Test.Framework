c0c1badd49471d6e72e3eb84eafd55659c168156
Govindarajan Panneerselvam <imgovind@live.com>
2014-11-16 00:12:24 -0600

    Split Munq into it's own project -- Removed unnecessary Munq references from test.framework.core's dependency list

---
+++ Test.Framework.Core.Munq/App_Start/Core.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Framework;
+
+namespace Test.Framework.Core.Munq
+{
+    public partial class AppInitalizer
+    {
+        public partial class Core
+        {
+            public static void Initialize()
+            {
+                Container.InitializeWith(new LightInjectTypeResolver());
+                Container.Register<IWebConfiguration, WebConfiguration>(ObjectLifeSpans.Singleton);
+            }
+        }
+    }
+}
---
+++ Test.Framework.Core.Munq/Content/App_Start/Core.cs.pp
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Test.Framework;
+
+namespace $rootnamespace$
+{
+    public partial class AppInitalizer
+    {
+        public partial class Core
+        {
+            public static void Initialize()
+            {
+                Container.InitializeWith(new MunqTypeResolver());
+                Container.Register<IWebConfiguration, WebConfiguration>(ObjectLifeSpans.Singleton);
+            }
+        }
+    }
+}
